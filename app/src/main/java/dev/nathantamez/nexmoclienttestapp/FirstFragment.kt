f617802d0ea785c4554f9b74b0baaa73650bdfd8
N Tamez <nathan.tamez@vonage.com>
2023-02-08 11:07:34 +0000

    feat: add token input

--- app/src/main/java/dev/nathantamez/nexmoclienttestapp/FirstFragment.kt
+++ app/src/main/java/dev/nathantamez/nexmoclienttestapp/FirstFragment.kt
@@ -34,8 +34,8 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NzU3ODIwMzIsImp0aSI6IjJiMTRiMTMwLWE2ZjgtMTFlZC04MTg4LTE3MDVjN2UxNWFhNiIsImFwcGxpY2F0aW9uX2lkIjoiNjZhMjUzNTEtNTc3Ni00NjQzLWIzMzItMDUzZDIxY2E0OGEyIiwic3ViIjoiYWxpY2UiLCJleHAiOjE2NzU3ODIwNTM5MjMsImFjbCI6eyJwYXRocyI6eyIvKi91c2Vycy8qKiI6e30sIi8qL2NvbnZlcnNhdGlvbnMvKioiOnt9LCIvKi9zZXNzaW9ucy8qKiI6e30sIi8qL2RldmljZXMvKioiOnt9LCIvKi9pbWFnZS8qKiI6e30sIi8qL21lZGlhLyoqIjp7fSwiLyovYXBwbGljYXRpb25zLyoqIjp7fSwiLyovcHVzaC8qKiI6e30sIi8qL2tub2NraW5nLyoqIjp7fSwiLyovbGVncy8qKiI6e319fX0.tuXH8fVUHxJMb4uTXh68msM1xOyGk-OMC4zuE6irNeey2_z-naHsg16UJourYfwUg80TDy2Y_Bq0lpFy2Q_gq9HZNanO4BDbNI_-R3CO5TeclIE3FPYc82u6MzVb7jsgY_nKaho7uGGwoL0NUwv2AUMzdOzD0NM1uK0yy94T2JoII6AMxWSNu_OPHdBIhOn2OSkDJnKHQ4Z9R5rH5FxuRxRbOeIB7DORcR1sjA2dBOIibLouUbECavGhIHs3wzSSbKbACEUbWh3HmQEWoaft8tXxZT-GfdrNAo9fALRJGC1Mx5bFG-pyQV3tveGPV5_HpWpuvddU9Z8TXLKc5dj0Rg"
         binding.loginButton.setOnClickListener {
+            val token = binding.tokenInput.text.toString()
             nexmoClient.login(token) { connectionStatus,_ ->
                 this.activity?.runOnUiThread {
                     when(connectionStatus) {
